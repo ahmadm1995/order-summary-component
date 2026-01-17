77c5a721a77b82ff034d4e59cb39e100a4af7fed
Ahmad <ahmad.mahmoud9523@gmail.com>
2026-01-17 18:07:07 -0500

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/order-summary-component',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
